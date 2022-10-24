e91e3f7f2c2d088cc03e8541785321f2638a227f
MexicanCacti <111838989+MexicanCacti@users.noreply.github.com>
2022-10-24 08:13:12 +0000

    Milo Dominguez

--- main.py
+++ main.py
@@ -14,5 +14,19 @@ names = ['Bill', 'Mary']
 # Make your code
 # ******************************
 
+fig, ax = plt.subplots()
+rect1 = ax.bar(x - width*1.5, Math, width, label='Math')
+rect2 = ax.bar(x - width/2, English, width, label='English')
+rect3 = ax.bar(x + width*1.5, Physics, width, label='Physics')
+rect4 = ax.bar(x + width/2 , Computer, width, label='Computer')
+
+
+ax.set_title('Grouped graph for scores')
+ax.legend()
+ax.set_xticks(x,names)
+ax.bar_label(rect1,Math)
+ax.bar_label(rect2,English)
+ax.bar_label(rect3,Physics)
+ax.bar_label(rect4,Computer)
 
 fig.savefig('A11.png')
